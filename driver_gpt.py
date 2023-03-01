696e2164e1243f4546fd5332097a6c01524022be
nineis7 <nineis7@163.com>
2023-03-01 07:09:11 +0000

    cuda update

--- driver_gpt.py
+++ driver_gpt.py
@@ -10,7 +10,9 @@ from transformers import AutoTokenizer, OpenAIGPTModel
 tokenizer = AutoTokenizer.from_pretrained("openai-gpt")
 model = OpenAIGPTModel.from_pretrained("openai-gpt", return_dict=False)
 model.eval() # Set the model in evaluation mode to deactivate the DropOut modules
-print(model)
+for p in model.parameters():
+    p.requires_grad_(False)
+#print(model)
 
 # 生成输入tensor，负责输入进trace中flow一遍得到trace后的计算图
 # Encode a text inputs
@@ -21,9 +23,24 @@ tokens_tensor = torch.tensor([indexed_tokens])
 
 # Creating the trace
 traced_model = torch.jit.trace(model, [tokens_tensor], strict=False)
-shape_list = [(i.debugName().split('.')[0], i.type().sizes()) for i in  list(traced_model.graph.inputs())[1:]]
-print(traced_model)
+traced_model.eval()
+for p in traced_model.parameters():
+    p.requires_grad_(False)
+    
+model.cuda()
+tt_c = tokens_tensor.cuda()
+res_pt = model(tt_c)
+torch.cuda.synchronize()
 
+def y():
+    for i in range(100):
+        model(tt_c)
+    torch.cuda.synchronize()
+
+y()
+
+'''
+shape_list = [(i.debugName().split('.')[0], i.type().sizes()) for i in  list(traced_model.graph.inputs())[1:]]
 import tvm.relay
 # parse pytorch model to tvm relay ir
 mod, params = tvm.relay.frontend.pytorch.from_pytorch(traced_model, shape_list, default_dtype="float32")
@@ -39,3 +56,4 @@ with tvm.transform.PassContext(opt_level=opt_level):
     print(mod)
 
 # add some changes
+'''
